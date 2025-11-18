0e9dd8d1509ee6b4991636e4e7d6fd25751653f1
nacht29 <nacht29.study@gmail.com>
2025-11-19 01:16:32 +0800

    feat: mini server working

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 
-(PORT, HOST) = sys.argv[0] if sys.argv[0] else 8888, sys.argv[1] if sys.argv[1] else ''
+HOST, PORT = '', 8888
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -14,12 +14,13 @@ while True:
 	request_data = client_connection.recv(1024)
 	print(request_data.decode('utf-8'))
 
+	content = "HELLO WORLD"
 	http_response = (
 		"HTTP/1.1 200 OK\r\n"
 		"Content-Type: text/plain\r\n"
-		"Content-Length: 11\r\n"
+		f"Content-Length: {len(content)}\r\n"
 		"\r\n"
-		"HELLO WORLD"
+		f"{content}"
 	)
 
 	client_connection.sendall(http_response.encode('utf-8'))
